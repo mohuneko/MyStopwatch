f52e0b59b5543c40840b9a0f3c3043b51c4ebe5c
mohuneko <b0wdmohu@gmail.com>
2020-08-15 20:02:28 +0900

    カウントアップ機能

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-'user scrict';
+'user strict';
 
 {
   const timer = document.getElementById('timer');
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
-  
+
+  let startTime;
+
+  function countUp() {
+    console.log(Date.now() - startTime);
+
+    setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+  });
 }
