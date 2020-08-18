d39d1a93e7ee1df1e735d0e03da7ad1c78a17b1b
mohuneko <b0wdmohu@gmail.com>
2020-08-18 17:49:26 +0900

    CSS

--- js/main.js
+++ js/main.js
@@ -22,17 +22,39 @@
     }, 10);
   }
 
+  function setButtonStateInitial() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+  function setButtonStateRunning() {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+  function setButtonStateStopped() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+
+  setButtonStateInitial();
+
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
-  clearTimeout(timeoutId);
-  elapsedTime += Date.now() - startTime;
+    setButtonStateStopped();
+    clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
-  timer.textContent = '00:00.000';
+    setButtonStateInitial();
+    timer.textContent = '00:00.000';
+    elapsedTime = 0;
   });
 }
